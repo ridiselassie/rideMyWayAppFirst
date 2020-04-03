05cf55bbda14d4998b74782378b12db5ddd22400
Rilwan Olajide Buari <rilwanbuari@gmail.com>
2020-04-03 23:10:15 +0100

    adjusted the fetch code in dashboard.js and renamed buttons in dashboard.ejs

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,4 +1,4 @@
-//Dashboard GET offers
+//Dashboard POST --- trying to post to the database(console.log for now), output the body of form at the same time on the front end
 
 document.getElementById('addData').addEventListener('submit', addData)
 
@@ -21,4 +21,23 @@ function addData(e){
 }
 
 
+//Dashboard GET..that is I posted some datas already with router.post and tried to fetch it back
 
+/*document.getElementById('getData').addEventListener('click', getData);
+
+function getData(){
+    fetch('/dashboard')
+    .then((res) => res.json())
+    .then(data) => {
+        let output = '<h2>Ride Requests</h2>';
+        data.forEach(function(ride){
+            output += `
+                <div>
+                    <p>${ride.destination}</p>
+                    <p>${ride.departure}</p>
+                </div>
+            `;
+        });
+        document.getElementById('output').innerHTML = output;
+    })
+} */
